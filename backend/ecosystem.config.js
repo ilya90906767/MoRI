0bdf16cd67ed1b82f403fd31e83a9669a0fb26f8
Ilya Betyaev <betyaev.ilya2004@gmail.com>
2025-03-22 11:44:25 +0300

    Set up start prod

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: 'backend', // Name of your application
+        script: 'manage.py',   // Entry point of your application
+        args: 'runserver',     // Command to run the Django development server
+        interpreter: '/root/Production/backend/.venv/bin/python3.10', // Path to the Python interpreter in your venv
+        env: {
+          DJANGO_SETTINGS_MODULE: 'backend.settings', // Your Django settings module
+          PYTHONPATH: '.',          // Path to your Django project
+        },
+      },
+    ],
+  };
