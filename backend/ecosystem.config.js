dcb4b3cee33583a1cba97f5d6f16f2b0a40740de
Ilya Betyaev <betyaev.ilya2004@gmail.com>
2025-03-22 13:07:26 +0300

    pm2 repair

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         name: 'backend', // Name of your application
         script: 'manage.py',   // Entry point of your application
         args: 'runserver',     // Command to run the Django development server
-        interpreter: '/root/website/backend/.venv/bin/python3.10', // Path to the Python interpreter in your venv
+        interpreter: '/root/website/.venv/bin/python3.10', // Path to the Python interpreter in your venv
         env: {
           DJANGO_SETTINGS_MODULE: 'backend.settings', // Your Django settings module
           PYTHONPATH: '.',          // Path to your Django project
